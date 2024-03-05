0b28a1bf0a0f935abb866e71628e9a45dad2872b
Quinton Miller <qjm412@gmail.com>
2024-03-05 00:41:51 -0500

    UniqueBuildingTest made, wont implement until getSchematic works properly

--- tt/src/test/java/tt/TileTest.java
+++ tt/src/test/java/tt/TileTest.java
@@ -3,6 +3,7 @@ package tt;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import tt.buildings.UniqueBuilding;
 
 import org.junit.Test;
---
+++ tt/src/test/java/tt/buildings/UniqueBuildingTest.java
@@ -0,0 +1,18 @@
+package tt.buildings;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import tt.buildings.UniqueBuilding;
+import tt.Building;
+
+import org.junit.Test;
+
+public class UniqueBuildingTest {
+    @Test
+    public void UniqueBuildingTest1 () {
+        Building actual = new UniqueBuilding();
+    }
+    
+}
