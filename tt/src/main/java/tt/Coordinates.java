12b4274d8993ad214384f5f2abd0c03e812d130e
Noah Shin <nds2529@g.rit.edu>
2024-03-07 00:02:51 -0500

    Added coordinates class

--- tt/src/main/java/tt/Coordinates.java
+++ tt/src/main/java/tt/Coordinates.java
@@ -1,5 +1,20 @@
 package tt;
 
 public class Coordinates {
+    private int x;
+    private int y;
+    public Coordinates(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+
 
 }
