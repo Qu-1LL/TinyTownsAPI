ae76d44970d47cf387a840bb4d037a5e38b353c0
Quinton Miller <qjm412@gmail.com>
2024-06-24 00:48:51 -0400

    TTDiscord first complete version. Testing and cleaning next

--- tt/src/main/java/tt/BuildingType.java
+++ tt/src/main/java/tt/BuildingType.java
@@ -1,19 +1,21 @@
 package tt;
 
 public enum BuildingType {
-    GREY("<:g_grey:1250992986100338779>"),
-    COTTAGE("<:h_cottage:1250865474837676164>"),
-    YELLOW("<:i_yellow:1250855270440112148>"),
-    ORANGE("<:j_orange:1250816696369942568>"),
-    RED("<:k_red:1250997229754253373>"),
-    GREEN("<:l_green:1250828044000559174>"),
-    NAVY("<:m_navy:1250861177454596167>"),
-    MONUMENT("<:n_monument:1251022844935278653>");
+    GREY("<:g_grey:1250992986100338779>","grey"),
+    COTTAGE("<:h_cottage:1250865474837676164>","cottage"),
+    YELLOW("<:i_yellow:1250855270440112148>","yellow"),
+    ORANGE("<:j_orange:1250816696369942568>","orange"),
+    RED("<:k_red:1250997229754253373>","red"),
+    GREEN("<:l_green:1250828044000559174>","green"),
+    NAVY("<:m_navy:1250861177454596167>","navy"),
+    MONUMENT("<:n_monument:1251022844935278653>","monument");
 
     private String emoji;
+    public String STRING;
 
-    private BuildingType (String emoji) {
+    private BuildingType (String emoji, String STRING) {
         this.emoji = emoji;
+        this.STRING = STRING;
     }
     @Override
     public String toString () {
--- tt/src/main/java/tt/jda/TTDiscord.java
+++ tt/src/main/java/tt/jda/TTDiscord.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.interaction.component.*;
@@ -16,29 +17,18 @@ import tt.*;
 public class TTDiscord implements TT {
 
     private final JDA jda;
-    private final String textChannelName;
-    private int playerCount;
     private Game game;
 
     private static final String TT = "tt";
     private static final String PLACE = "place";
     private static final String BUILD = "build";
+    private static final String DONE = "done";
     private static final String CHANNEL_ID = "1214007594151321701";
 
-    public TTDiscord (JDA jda, String textChannelName, Game game) {
+    public TTDiscord (JDA jda, Game game) {
         this.jda = jda;
-        this.textChannelName = textChannelName;
         this.game = game;
     }
-
-    // @Override
-    // public void onMessageReceived(MessageReceivedEvent event) {
-
-    //     if (!parsedMessage[0].equalsIgnoreCase(TT)) {
-    //         return;
-    //     }
-
-    // }
 
     public class ResourceListener extends ListenerAdapter {
         Resource resource = null;
@@ -191,6 +181,7 @@ public class TTDiscord implements TT {
             this.placed = new HashSet<String>();
             this.placeableResources = placeableResources;
             this.playersMap = playersMap;
+            jda.addEventListener(this);
         }
         @Override
         public void onMessageReceived(MessageReceivedEvent event) {
@@ -209,7 +200,90 @@ public class TTDiscord implements TT {
             if (!(x < 4 && x > 0 && y < 4 && y > 0)) {
                 return;
             } 
-            if (placeableResources.containsKey(event.getAuthor().getId()));
+            String eventsName = event.getAuthor().getId();
+            if (placeableResources.containsKey(eventsName)) {
+                playersMap.get(eventsName).place(placeableResources.get(eventsName),x,y);
+                placed.add(eventsName);
+                jda.getTextChannelById(CHANNEL_ID).sendMessage(eventsName + "'s town:").queue();
+                jda.getTextChannelById(CHANNEL_ID).sendMessage(playersMap.get(eventsName).getTown().toString()).queue();
+            }
+            if (placed.size() == playersMap.size()) {
+                complete = true;
+                jda.removeEventListener(this);
+            }
+        }
+        public boolean complete () {
+            return complete;
+        }
+    }
+
+    public class BuildListener extends ListenerAdapter {
+        private boolean complete;
+        private HashSet<String> done;
+        private HashMap<String, Player> playersMap;
+        private boolean built;
+
+        public BuildListener (HashMap<String,Player> playersMap) {
+            this.complete = false;
+            this.done = new HashSet<String>();
+            this.playersMap = playersMap;
+            jda.addEventListener(this);
+        }
+        @Override
+        public void onMessageReceived (MessageReceivedEvent event) {
+            String[] parsedMessage = event.getMessage().getContentRaw().split(" ");
+            if (!parsedMessage[0].equalsIgnoreCase(TT)) {
+                return;
+            }
+            if (!parsedMessage[1].equalsIgnoreCase(BUILD) && !parsedMessage[1].equalsIgnoreCase(DONE)) {
+                return;
+            }
+            String author = event.getAuthor().getId();
+            if (parsedMessage[1].equalsIgnoreCase(DONE)) {
+                if (playersMap.get(author).getEmptyTiles().size() == 0) {
+                    jda.getTextChannelById(CHANNEL_ID).sendMessage(author + " must place a building.").queue();
+                } else {
+                    done.add(author);
+                    playersMap.remove(author);
+                    if (playersMap.size() == 0) {
+                        complete = true;
+                        jda.removeEventListener(this);
+                    }
+                    event.getMessage().addReaction(event.getGuild().getEmojiById("<:a_empty:1250660017015619664>")).queue();
+                }
+                return;
+            }
+            if (parsedMessage.length != 5) {
+                return;
+            }
+            int index = 0;
+            for (BuildingType bt : BuildingType.values()) {
+                if (parsedMessage[2] == bt.STRING) {
+                    break;
+                }
+                if (index == 7) {
+                    return;
+                }
+                index++;
+            }
+            int x = Integer.parseInt(parsedMessage[3]);
+            int y = Integer.parseInt(parsedMessage[4]);
+            if (!(x < 4 && x > 0 && y < 4 && y > 0)) {
+                return;
+            } 
+            if(playersMap.get(author).build(x,y,index)) {
+                jda.getTextChannelById(CHANNEL_ID).sendMessage(author + " built in their town:").queue();
+                jda.getTextChannelById(CHANNEL_ID).sendMessage(playersMap.get(author).getTown().toString()).queue();
+                built = true;
+            } else {
+                jda.getTextChannelById(CHANNEL_ID).sendMessage("Could not complete the latest build requested by " + author).queue();
+            }
+        }
+        public boolean complete () {
+            return complete;
+        }
+        public boolean built () {
+            return built;
         }
     }
 
@@ -243,13 +317,38 @@ public class TTDiscord implements TT {
 
     @Override
     public void placeResources(HashMap<String, Resource> placeableResources, HashMap<String,Player> playersMap) {
+        PlaceListener receiver = new PlaceListener(placeableResources, playersMap);
+        Thread thread = new Thread() {
+            @Override
+            public void run () {
+                while (!receiver.complete()) {
+                    try {
+                        sleep(100);
+                    } catch (InterruptedException e) {}
+                }
+            }
+        };
         
-
+        thread.run();
     }
 
     @Override
     public boolean freeBuild(HashMap<String, Player> playersMap) {
-        return false;
+        BuildListener receiver = new BuildListener(playersMap);
+        Thread thread = new Thread() {
+            @Override
+            public void run () {
+                while (!receiver.complete()) {
+                    try {
+                        sleep(100);
+                    } catch (InterruptedException e) {}
+                }
+            }
+        };
+        
+        thread.run();
+
+        return receiver.built();
     }
 
     @Override
@@ -274,7 +373,11 @@ public class TTDiscord implements TT {
 
     @Override
     public void gameEnd() {
-        // TODO Auto-generated method stub
-        
+        TextChannel tc = jda.getTextChannelById(CHANNEL_ID);
+        tc.sendMessage("And the game is over!\nHere are your scores!").queue();
+        for (Player player : game.getPlayersArray()) {
+            tc.sendMessage(player.getName() + ":" + player.getTown().score()).queue();
+            tc.sendMessage(player.getTown().toString()).queue();
+        }
     }
 }
